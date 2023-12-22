e9483e47a516886c94520480dce9f80ccdb56109
Keyan <135648468+keyan74@users.noreply.github.com>
2023-12-22 22:28:39 +0530

    Update get_instance_name.py

--- get_instance_name.py
+++ get_instance_name.py
@@ -1,7 +1,7 @@
 import boto3
 
 # AWS credentials and region
-aws_profile = 'skyvera'
+aws_profile = 'keyan'
 aws_region = 'us-east-1'
 
 # Initialize Boto3 EC2 client
@@ -9,33 +9,9 @@ session = boto3.Session(profile_name=aws_profile, region_name=aws_region)
 ec2_client = session.client('ec2')
 
 # List of instance IDs for which you want to retrieve instance names
-instance_ids_to_query = ['i-0bba723c853422427',
-'i-02d49ece76cf265ba',
-'i-038b30ed1d77a156f',
-'i-004fd3cb95953cae7',
-'i-021b5e47dcb2fac5b',
-'i-006ff9f7fcc9acf62',
-'i-09bb2983b1a7c0a67',
-'i-07e4d66e4ddbfa8fa',
-'i-096eea64b67a522c3',
-'i-036acb7b995b7f386',
-'i-0b0bc52800dbe5aae',
-'i-0ddd086876b35eb09',
-'i-0eade07c17be0c160',
-'i-08adcfcaa481900e5',
-'i-081e574e0228177ad',
-'i-05d3e77815171420d',
-'i-00896b4dde86ef071',
-'i-0f468504d8e017cc7',
-'i-091b0c398605e9f07',
-'i-094e0d361e522576f',
-'i-0a0dc3218e8e99370',
-'i-0d43add73b3f70746',
-'i-0fb8280840d7b1093',
-'i-0f75e1ae03df3f19d',
-'i-0d6a123b8c650e7d5',
-'i-067d9c5e281338c0d',
-]  # Replace with your instance IDs
+instance_ids_to_query = ['i-098sdesd002033030',
+                        ] 
+# Replace with your instance IDs
 
 # Get instance details
 response = ec2_client.describe_instances(InstanceIds=instance_ids_to_query)
