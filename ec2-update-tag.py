d471c6d84d3d71caa64a44cc56c4cdddd0bcc1f4
Karthikeyan <135648468+keyan74@users.noreply.github.com>
2023-12-27 20:46:22 +0530

    Update ec2-update-tag.py

--- ec2-update-tag.py
+++ ec2-update-tag.py
@@ -1,17 +1,17 @@
 import boto3
-dev = boto3.Session(profile_name="skyvera", region_name="us-east-1")
+dev = boto3.Session(profile_name="keyan", region_name="us-east-1")
 
 client = dev.client('ec2', region_name='us-east-1')
 response = client.create_tags(
     Resources=[
-'i-0ce4ad83e4407f7cf',
-'i-00f73ab9e21ca731a',
+'i-0xcccxxxxcf',
+'i-cdzzzzxdd33',
 
     ],
     Tags=[
         {
             'Key': 'Owner',
-            'Value': 'nirmalkumar@devfactory.local'
+            'Value': 'XXXXxX@XX.com'
 
         },
     ]
